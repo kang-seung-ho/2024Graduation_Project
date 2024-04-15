baa5252b75d6a5df1c8a5e515ddbde51850de3bc
iconstudio <yoyofa2@hotmail.com>
2024-04-15 23:25:10 +0900

    서버에서 캐릭터 생성 메시지 송신

--- Server/Demo/FrameworkEvents6.cpp
+++ Server/Demo/FrameworkEvents6.cpp
@@ -18,3 +18,56 @@ if (not io) \
 	return true; \
 }()
 module Demo.Framework;
+
+bool
+demo::Framework::OnCreatingCharacters(iconer::app::Room& room)
+{
+	auto number = room.GetMembersCount();
+	if (number == 0)
+	{
+		return false;
+	}
+
+	auto& cnt_ref = room.proceedMemberCount;
+	if (not cnt_ref.CompareAndSet(number - 1, 0, std::memory_order_acq_rel))
+	{
+		return false;
+	}
+
+	auto cnt = cnt_ref.Load(std::memory_order_acquire);
+
+	room.ForEach
+	(
+		[&](iconer::app::User& member)
+		{
+			auto [io, ctx] = member.SendCreateCharactersPacket(); 
+			if (not io)
+			{
+				ctx.Complete();
+			}
+
+			cnt = cnt_ref.FetchAdd(1, std::memory_order_release) + 1;
+		}
+	);
+
+#if false
+	if (cnt == 0)
+	{
+		return false;
+	}
+#else
+	if (0 < cnt)
+	{
+		return true;
+	}
+#endif
+
+	return false;
+}
+
+void
+demo::Framework::OnFailedToCreateCharacters(iconer::app::Room& room)
+noexcept
+{
+
+}
