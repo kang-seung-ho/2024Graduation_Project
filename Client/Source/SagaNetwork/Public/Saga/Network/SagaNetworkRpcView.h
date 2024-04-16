2525c52c698ca269c3c0636156d2ef753e9e3a44
iconstudio <yoyofa2@hotmail.com>
2024-04-17 05:14:37 +0900

    RPC 이벤트 뷰 인터페이스 클래스 추가

---
+++ Client/Source/SagaNetwork/Public/Saga/Network/SagaNetworkRpcView.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+
+#include "SagaNetworkRpcView.generated.h"
+
+UINTERFACE(MinimalAPI)
+class USagaNetworkRpcView : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class SAGANETWORK_API ISagaNetworkRpcView
+{
+	GENERATED_BODY()
+
+public:
+};
