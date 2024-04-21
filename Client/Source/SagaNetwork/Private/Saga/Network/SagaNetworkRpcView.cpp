3ede693c7f1ffb7412705be887db346f9b3a5494
iconstudio <yoyofa2@hotmail.com>
2024-04-21 21:19:23 +0900

    기본적인 RPC 송신 구현

--- Client/Source/SagaNetwork/Private/Saga/Network/SagaNetworkRpcView.cpp
+++ Client/Source/SagaNetwork/Private/Saga/Network/SagaNetworkRpcView.cpp
@@ -22,3 +22,91 @@ ISagaNetworkRpcView::EndWalk()
 		system->SendRpcPacket(ESagaRpcProtocol::RPC_END_WALK);
 	}
 }
+
+void
+ISagaNetworkRpcView::BeginRun()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_BEG_RUN);
+	}
+}
+
+void
+ISagaNetworkRpcView::EndRun()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_END_RUN);
+	}
+}
+
+void
+ISagaNetworkRpcView::BeginJump()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_BEG_JUMP);
+	}
+}
+
+void
+ISagaNetworkRpcView::EndJump()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_END_JUMP);
+	}
+}
+
+void
+ISagaNetworkRpcView::BeginAttack()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_BEG_ATTACK_0);
+	}
+}
+
+void
+ISagaNetworkRpcView::EndAttack()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_END_ATTACK_0);
+	}
+}
+
+void
+ISagaNetworkRpcView::BeginRide()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_BEG_RIDE);
+	}
+}
+
+void
+ISagaNetworkRpcView::EndRide()
+{
+	auto system = GEngine->GetEngineSubsystem<USagaNetworkSubSystem>();
+
+	if (GetRpcID() != -1)
+	{
+		system->SendRpcPacket(ESagaRpcProtocol::RPC_END_RIDE);
+	}
+}
