b3777063fd69f1fd5410b705369d5932faf426d5
iconstudio <yoyofa2@hotmail.com>
2024-05-08 07:11:48 +0900

    안 쓰는 송신 코드 삭제

--- Server/Demo/FrameworkEvents5.cpp
+++ Server/Demo/FrameworkEvents5.cpp
@@ -1,22 +1,4 @@
 module;
-#include <string_view>
-
-#define SEND(user_var, method, ...) \
-auto [io, ctx] = ((user_var).method)(__VA_ARGS__); \
-if (not io) \
-{ \
-	ctx.Complete(); \
-}
-#define SEND_RESULT(captures, user_var, method, ...) \
-[captures](){ \
-	auto [io, ctx] = ((user_var).method)(__VA_ARGS__); \
-	if (not io) \
-	{ \
-		ctx.Complete(); \
-		return false; \
-	} \
-	return true; \
-}()
 module Demo.Framework;
 
 using namespace iconer::app;
