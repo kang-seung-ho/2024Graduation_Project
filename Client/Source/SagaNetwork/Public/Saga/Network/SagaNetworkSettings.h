afbb86adc78d51c419dadef1320e8f6aabbbd2e6
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-05-10 00:09:56 +0900

    네트워크모드 복구

--- Client/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSettings.h
+++ Client/Source/SagaNetwork/Public/Saga/Network/SagaNetworkSettings.h
@@ -8,5 +8,5 @@ namespace saga
 	inline const FString RemoteAddress = TEXT("61.84.90.149");
 	inline constexpr int32 RemotePort = 40000U;
 	inline constexpr int32 LocalPort = 40001U;
-	inline constexpr bool IsOfflineMode = true;
+	inline constexpr bool IsOfflineMode = false;
 };
