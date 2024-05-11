1a084b6523d2701574321431b2fc6b200ba6f870
iconstudio <yoyofa2@hotmail.com>
2024-05-11 19:38:45 +0900

    캐릭터 선택 레벨에서는 서브시스템이 RPC를 받음

--- Client/Source/SagaNetwork/Private/Saga/Network/SagaNetworkSubSystemRpcEvents.cpp
+++ Client/Source/SagaNetwork/Private/Saga/Network/SagaNetworkSubSystemRpcEvents.cpp
@@ -2,4 +2,36 @@
 
 void
 USagaNetworkSubSystem::OnRpc_Implementation(ESagaRpcProtocol cat, int32 id, int64 arg0, int32 arg1)
-{}
+{
+	if (id == GetLocalUserId()) // 로컬 클라이언트
+	{
+#pragma region RPC from Local Client
+
+#pragma endregion
+	}
+	else // 원격 클라이언트
+	{
+#pragma region RPC from Remote Client
+
+#pragma endregion
+	}
+
+	switch (cat)
+	{
+	case ESagaRpcProtocol::RPC_MAIN_WEAPON:
+	{
+		const auto new_weapon = static_cast<EPlayerWeapon>(arg0);
+		SetWeapon(id, new_weapon);
+
+		EPlayerWeapon test{};
+		GetWeapon(id, test);
+		ensure(new_weapon == test);
+
+		UE_LOG(LogSagaNetwork, Log, TEXT("[RPC] Client %d's weapon is %d"), id, static_cast<int>(test));
+	}
+	break;
+
+	default:
+		break;
+	}
+}
