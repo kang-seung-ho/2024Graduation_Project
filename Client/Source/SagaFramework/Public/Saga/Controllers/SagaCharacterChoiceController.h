2d1b1e4978d906f746c6a1713e47abd8f1bf6e52
iconstudio <yoyofa2@hotmail.com>
2024-05-24 16:43:29 +0900

    안 쓰는 플레이어 컨트롤러의 속성 제거

--- Client/Source/SagaFramework/Public/Saga/Controllers/SagaCharacterChoiceController.h
+++ Client/Source/SagaFramework/Public/Saga/Controllers/SagaCharacterChoiceController.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Saga/Controllers/SagaInterfaceController.h"
-#include <InputActionValue.h>
 
 #include "SagaCharacterChoiceController.generated.h"
 
@@ -10,49 +9,4 @@ class SAGAFRAMEWORK_API ASagaCharacterChoiceController : public ASagaInterfaceCo
 	GENERATED_BODY()
 
 public:
-	//UPROPERTY()
-	TSubclassOf<UUserWidget> mSelectWidgetClass;
-	//UPROPERTY()
-	class USagaCharacterSelectWidget* mSelectWidget;
-
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	AActor* myHoveredCharacter;
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	AActor* mySelectedCharacter;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	bool isGameStartable;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	bool isStartButtonClicked;
-
-	UPROPERTY(BlueprintReadOnly)
-	int32 Minutes = 0;
-	UPROPERTY(BlueprintReadOnly)
-	int32 Seconds = 30;
-
-	ASagaCharacterChoiceController(const FObjectInitializer& initializer);
-
-	virtual void Tick(float delta_time) override;
-
-	UFUNCTION()
-	void CountDown();
-
-	UFUNCTION()
-	bool GetStartButtonStatus() const noexcept
-	{
-		return isStartButtonClicked;
-	}
-
-	UFUNCTION()
-	AActor* GetSelectedActor() const noexcept
-	{
-		return mySelectedCharacter;
-	}
-
-	UFUNCTION()
-	void OnClick(const FInputActionValue& input);
-
-protected:
-	virtual void BeginPlay() override;
-	virtual void SetupInputComponent() override;
 };
