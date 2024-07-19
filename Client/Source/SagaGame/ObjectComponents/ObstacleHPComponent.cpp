349356c352df26b13d464b58a98c2723782cab85
iconstudio <yoyofa2@hotmail.com>
2024-07-19 21:21:22 +0900

    장애물 체력 컴포넌트의 코드 정리

--- Client/Source/SagaGame/ObjectComponents/ObstacleHPComponent.cpp
+++ Client/Source/SagaGame/ObjectComponents/ObstacleHPComponent.cpp
@@ -1,46 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ObjectComponents/ObstacleHPComponent.h"
+#include <UObject/Object.h>
+#include <Templates/Casts.h>
+
 #include "Obstacles/MapObstacle1.h"
 
-// Sets default values for this component's properties
 UObstacleHPComponent::UObstacleHPComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-
 }
-
-
-// Called when the game starts
-void UObstacleHPComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-}
-
 
 // Called every frame
 void UObstacleHPComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
 }
 
-void UObstacleHPComponent::TakeDamage(float DamageAmount)
+void
+UObstacleHPComponent::TakeDamage(float DamageAmount)
 {
 	Health -= DamageAmount;
 	CheckDeath();
 }
 
-float UObstacleHPComponent::GetCurrentHealth()
+float
+UObstacleHPComponent::GetCurrentHealth()
+const
 {
 	return Health;
 }
 
-void UObstacleHPComponent::SetObjectHealth(float hp)
+void
+UObstacleHPComponent::SetObjectHealth(float hp)
 {
 	Health = hp;
 }
@@ -65,34 +55,43 @@ void UObstacleHPComponent::CheckDeath()
 {
 	if (Health <= 0)
 	{
-		AActor* ThisObstacleActor = GetOwner();
-		if (ThisObstacleActor)
+		AActor* const owner = GetOwner();
+
+		if (IsValid(owner))
 		{
-			ThisObstacleActor->SetActorEnableCollision(false);
-			ThisObstacleActor->FindComponentByClass<UPrimitiveComponent>()->SetSimulatePhysics(true);
-			// Spawn the item box
-			AMapObstacle1* Obstacle = Cast<AMapObstacle1>(ThisObstacleActor);
-			if (Obstacle)
+			owner->SetActorEnableCollision(false);
+			owner->FindComponentByClass<UPrimitiveComponent>()->SetSimulatePhysics(true);
+
+			/*
+			Offline Mode:
+			* Create a item box now.
+
+			Online  Mode:
+			* Defer the creation of an item box to send rpc.
+			*/
+			if (AMapObstacle1* Obstacle = Cast<AMapObstacle1>(owner))
 			{
 				Obstacle->SpawnItemBox();
 			}
 
-			// Set Timer
 			GetWorld()->GetTimerManager().SetTimer(DestructionTimerHandle, this, &UObstacleHPComponent::HandleDestruction, DestructionDelay);
 		}
 	}
 }
 
-void UObstacleHPComponent::HandleDestruction()
+void
+UObstacleHPComponent::HandleDestruction()
 {
 	// Call the function to remove the chaos destruction pieces
 	RemoveDestructionPieces();
 }
 
-void UObstacleHPComponent::RemoveDestructionPieces()
+void
+UObstacleHPComponent::RemoveDestructionPieces()
 {
-	AActor* ThisObestacleActor = GetOwner();
-	if (ThisObestacleActor)
+	AActor* const ThisObestacleActor = GetOwner();
+
+	if (IsValid(ThisObestacleActor))
 	{
 		ThisObestacleActor->Destroy();
 	}
