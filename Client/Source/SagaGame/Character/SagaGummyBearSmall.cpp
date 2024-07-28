e32d94fb7d29fc6250935c1e781fa0c3e2a20232
iconstudio <yoyofa2@hotmail.com>
2024-07-28 18:36:03 +0900

    서식 수정

--- Client/Source/SagaGame/Character/SagaGummyBearSmall.cpp
+++ Client/Source/SagaGame/Character/SagaGummyBearSmall.cpp
@@ -72,24 +72,25 @@ void ASagaGummyBearSmall::BeginPlay()
 	//mAnimInst = Cast<UAIMonsterAnimInstance>(mMesh->GetAnimInstance());
 }
 
-// Called every frame
-void ASagaGummyBearSmall::Tick(float DeltaTime)
+void
+ASagaGummyBearSmall::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-void ASagaGummyBearSmall::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void
+ASagaGummyBearSmall::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if (OtherActor && OtherActor != this && OtherActor->IsA(ASagaPlayableCharacter::StaticClass()))
 	{
 		HandleExplosionAndDamage(OtherActor);
 		Destroy();
 	}
-
 }
 
-void ASagaGummyBearSmall::HandleExplosionAndDamage(AActor* OtherActor)
+void
+ASagaGummyBearSmall::HandleExplosionAndDamage(AActor* OtherActor)
 {
 	// Play explosion effect
 	if (ExplosionEffect)
