de34a579284da0e87fa637d0bfecdd84d925a202
iconstudio <yoyofa2@hotmail.com>
2024-08-01 11:13:36 +0900

    장애물 코드 정리

--- Client/Source/SagaGame/Obstacles/SagaDestructibleMapObstacle.cpp
+++ Client/Source/SagaGame/Obstacles/SagaDestructibleMapObstacle.cpp
@@ -6,123 +6,133 @@
 
 ASagaDestructibleMapObstacle::ASagaDestructibleMapObstacle()
 {
-    PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;
 
-    // Initialize components
-    MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComponent"));
+	// Initialize components
+	MeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComponent"));
 
 
-    static ConstructorHelpers::FObjectFinder<USkeletalMesh> ObstacleMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Terrains/Modelings/Pinata/Pinata.Pinata'"));
-    if (ObstacleMeshRef.Succeeded())
-    {
-        MeshComponent->SetSkeletalMesh(ObstacleMeshRef.Object);
-    }
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> ObstacleMeshRef(TEXT("/Script/Engine.SkeletalMesh'/Game/Terrains/Modelings/Pinata/Pinata.Pinata'"));
+	if (ObstacleMeshRef.Succeeded())
+	{
+		MeshComponent->SetSkeletalMesh(ObstacleMeshRef.Object);
+	}
 
-    static ConstructorHelpers::FObjectFinder<UAnimSequence> DeathAnimFinder(TEXT("/Script/Engine.AnimSequence'/Game/Terrains/Modelings/Pinata/Pinata_Anim_Bomb.Pinata_Anim_Bomb'"));
-    if (DeathAnimFinder.Succeeded())
-    {
-        DeathAnimation = DeathAnimFinder.Object;
-    }
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> DeathAnimFinder(TEXT("/Script/Engine.AnimSequence'/Game/Terrains/Modelings/Pinata/Pinata_Anim_Bomb.Pinata_Anim_Bomb'"));
+	if (DeathAnimFinder.Succeeded())
+	{
+		DeathAnimation = DeathAnimFinder.Object;
+	}
 
-    static ConstructorHelpers::FObjectFinder<UAnimSequence> HitAnimRef(TEXT("/Script/Engine.AnimSequence'/Game/Terrains/Modelings/Pinata/Pinata_Anim_Hit_01.Pinata_Anim_Hit_01'"));
-    if (HitAnimRef.Succeeded())
-    {
-        HitAnimation = HitAnimRef.Object;
-    }
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> HitAnimRef(TEXT("/Script/Engine.AnimSequence'/Game/Terrains/Modelings/Pinata/Pinata_Anim_Hit_01.Pinata_Anim_Hit_01'"));
+	if (HitAnimRef.Succeeded())
+	{
+		HitAnimation = HitAnimRef.Object;
+	}
 
-    static ConstructorHelpers::FObjectFinder<UNiagaraSystem> NiagaraEffect(TEXT("/Script/Niagara.NiagaraSystem'/Game/VFX/PartyFX/Niagara/NS_Ribbon_Party_2.NS_Ribbon_Party_2'"));
-    if (NiagaraEffect.Succeeded())
-    {
-        HitEffect = NiagaraEffect.Object;
-        UE_LOG(LogTemp, Warning, TEXT("Niagara Effect Loaded"));
-    }
-    else
-    {
-        UE_LOG(LogTemp, Warning, TEXT("Niagara Effect Not Loaded"));
-    }
+	static ConstructorHelpers::FObjectFinder<UNiagaraSystem> NiagaraEffect(TEXT("/Script/Niagara.NiagaraSystem'/Game/VFX/PartyFX/Niagara/NS_Ribbon_Party_2.NS_Ribbon_Party_2'"));
+	if (NiagaraEffect.Succeeded())
+	{
+		HitEffect = NiagaraEffect.Object;
+		UE_LOG(LogTemp, Warning, TEXT("Niagara Effect Loaded"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Niagara Effect Not Loaded"));
+	}
 
-    // Set up Mesh component as a Root Component
-    RootComponent = MeshComponent;
+	// Set up Mesh component as a Root Component
+	RootComponent = MeshComponent;
 
-    MeshComponent->SetCollisionProfileName(TEXT("Enemy")); // Collision Enabled for both Red and Blue Teams
-    SetActorScale3D(FVector(0.5f, 0.5f, 0.5f));
+	MeshComponent->SetCollisionProfileName(TEXT("Enemy")); // Collision Enabled for both Red and Blue Teams
+	SetActorScale3D(FVector(0.5f, 0.5f, 0.5f));
 }
 
-void ASagaDestructibleMapObstacle::BeginPlay()
+float
+ASagaDestructibleMapObstacle::TakeDamage(float dmg, FDamageEvent const& event, AController* instigator, AActor* causer)
 {
-    Super::BeginPlay();
-}
+	float DamageApplied = Super::TakeDamage(dmg, event, instigator, causer);
 
-// Called every frame
-void ASagaDestructibleMapObstacle::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
-}
+	UE_LOG(LogTemp, Warning, TEXT("Obstacle took %f damage, current health: %f"), dmg, myHealth);
 
-void ASagaDestructibleMapObstacle::DestroyObstacle()
-{
-    Destroy();
-}
+	myHealth -= DamageApplied;
 
-void ASagaDestructibleMapObstacle::ChangeLevel()
-{
-    UGameplayStatics::OpenLevel(this, FName("GameEndLevel"));
-}
+	const auto world = GetWorld();
+	auto& timer = GetWorldTimerManager();
+	const auto sys = USagaGameSubsystem::GetSubSystem(world);
+	const auto net = USagaGameSubsystem::GetSubSystem(world);
 
-float ASagaDestructibleMapObstacle::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
-{
-    float DamageApplied = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	if (myHealth <= 0)
+	{
+		FVector SpawnLocation = GetActorLocation() + FVector(0.0f, 0.0f, 40.0f);
+		FRotator SpawnRotation = FRotator::ZeroRotator;
+		FActorSpawnParameters SpawnParameters;
 
-    UE_LOG(LogTemp, Warning, TEXT("Obstacle took %f damage, current health: %f"), DamageAmount, myHealth);
+		if (DeathAnimation && MeshComponent)
+		{
+			MeshComponent->PlayAnimation(DeathAnimation, false);
+		}
 
-    myHealth -= DamageApplied;
+		FVector NiagaraSpawnLocation = GetActorLocation() + FVector(0.0f, 0.0f, 100.0f);
+		FRotator NiagaraSpawnRotation = GetActorRotation();
+		UNiagaraComponent* NiagaraComponent = UNiagaraFunctionLibrary::SpawnSystemAttached(HitEffect, RootComponent, NAME_None, NiagaraSpawnLocation, NiagaraSpawnRotation, EAttachLocation::KeepWorldPosition, false);
 
-    if (myHealth <= 0)
-    {
-        FVector SpawnLocation = GetActorLocation() + FVector(0.0f, 0.0f, 40.0f);
-        FRotator SpawnRotation = FRotator::ZeroRotator;
-        FActorSpawnParameters SpawnParameters;
+		if (NiagaraComponent)
+		{
+			// 3초 후에 나이아가라 이펙트 정지
+			FTimerHandle NiagaraTimerHandle;
+			timer.SetTimer(NiagaraTimerHandle, NiagaraComponent, &UNiagaraComponent::Deactivate, 3.0f, false);
+		}
 
-        if (DeathAnimation && MeshComponent)
-        {
-            MeshComponent->PlayAnimation(DeathAnimation, false);
-        }
+		// Uncomment or modify this line as necessary to spawn an item
+		// world->SpawnActor<ASagaItemBox>(ASagaItemBox::StaticClass(), SpawnLocation, SpawnRotation, SpawnParameters);
 
-        FVector NiagaraSpawnLocation = GetActorLocation() + FVector(0.0f, 0.0f, 100.0f);
-        FRotator NiagaraSpawnRotation = GetActorRotation();
-        UNiagaraComponent* NiagaraComponent = UNiagaraFunctionLibrary::SpawnSystemAttached(HitEffect, RootComponent, NAME_None, NiagaraSpawnLocation, NiagaraSpawnRotation, EAttachLocation::KeepWorldPosition, false);
+		// Set a timer to destroy the obstacle after 3 seconds
+		timer.SetTimer(TimerHandle, this, &ASagaDestructibleMapObstacle::DestroyObstacle, 4.0f, false);
 
-        if (NiagaraComponent)
-        {
-            // 3초 후에 나이아가라 이펙트 정지
-            FTimerHandle NiagaraTimerHandle;
-            GetWorldTimerManager().SetTimer(NiagaraTimerHandle, NiagaraComponent, &UNiagaraComponent::Deactivate, 3.0f, false);
-        }
+		timer.SetTimer(LevelChangeTimerHandle, this, &ASagaDestructibleMapObstacle::ChangeLevel, 3.0f, false);
 
-        // Uncomment or modify this line as necessary to spawn an item
-        // GetWorld()->SpawnActor<ASagaItemBox>(ASagaItemBox::StaticClass(), SpawnLocation, SpawnRotation, SpawnParameters);
-
-        // Set a timer to destroy the obstacle after 3 seconds
-        GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ASagaDestructibleMapObstacle::DestroyObstacle, 4.0f, false);
-
-        GetWorld()->GetTimerManager().SetTimer(LevelChangeTimerHandle, this, &ASagaDestructibleMapObstacle::ChangeLevel, 3.0f, false);
 		if (TeamPinataColor == 0)
 		{
-			USagaGameSubsystem::GetSubSystem(GetWorld())->SetWhoWonByPinata(0);
+			sys->SetWhoWonByPinata(0);
 		}
 		else
 		{
-			USagaGameSubsystem::GetSubSystem(GetWorld())->SetWhoWonByPinata(1);
+			sys->SetWhoWonByPinata(1);
 		}
+	}
+	else
+	{
+		if (HitAnimation && MeshComponent)
+		{
+			MeshComponent->PlayAnimation(HitAnimation, false);
+		}
+	}
 
-    }
-    else
-    {
-        if (HitAnimation && MeshComponent)
-        {
-            MeshComponent->PlayAnimation(HitAnimation, false);
-        }
-    }
+	return DamageApplied;
+}
 
-    return DamageApplied;
+void
+ASagaDestructibleMapObstacle::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void
+ASagaDestructibleMapObstacle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void
+ASagaDestructibleMapObstacle::DestroyObstacle()
+{
+	Destroy();
+}
+
+void
+ASagaDestructibleMapObstacle::ChangeLevel()
+{
+	UGameplayStatics::OpenLevel(this, FName("GameEndLevel"));
 }
