b2fbed53ab27df1f9cf55fdefc9f22d6194a14f3
강승호 <112061429+kang-seung-ho@users.noreply.github.com>
2024-08-03 12:07:07 +0900

    Revised for weaponchoice level's character weapon and animation

--- Client/Source/SagaGame/CharacterChoice/SagaSelectCharacter.cpp
+++ Client/Source/SagaGame/CharacterChoice/SagaSelectCharacter.cpp
@@ -7,7 +7,7 @@
 ASagaSelectCharacter::ASagaSelectCharacter()
 	: Super()
 	, mBody(), mMesh()
-	, humanCharacterAnimation()
+	//, humanCharacterAnimation()
 	, mWeapon()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -18,6 +18,12 @@ ASagaSelectCharacter::ASagaSelectCharacter()
 	mBody->SetCollisionProfileName(TEXT("Player"));
 
 	mMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/PlayerAssets/Player.Player'"));
+	if (MeshAsset.Succeeded())
+	{
+		mMesh->SetSkeletalMesh(MeshAsset.Object);
+	}
+
 	mMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 	mMesh->SetupAttachment(mBody);
 
@@ -26,6 +32,16 @@ ASagaSelectCharacter::ASagaSelectCharacter()
 	{
 		humanCharacterAnimation = AnimAsset.Class;
 	}
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+
+	WeaponMesh->SetupAttachment(mMesh);
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Weapon"));
+	if (mMesh)
+	{
+		WeaponMesh->SetupAttachment(mMesh, TEXT("c_middle1_r"));
+	}
 }
 
 void
@@ -33,5 +49,31 @@ ASagaSelectCharacter::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 
-	mMesh->SetAnimInstanceClass(humanCharacterAnimation.LoadSynchronous());
+	//mMesh->SetAnimInstanceClass(humanCharacterAnimation.LoadSynchronous());
 }
+
+#if WITH_EDITOR
+void ASagaSelectCharacter::PostEditChangeChainProperty(FPropertyChangedChainEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeChainProperty(PropertyChangedEvent);
+
+	const FName PropertyName = (PropertyChangedEvent.Property != nullptr) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(ASagaSelectCharacter, humanCharacterAnimation))
+	{
+		if (humanCharacterAnimation.IsValid())
+		{
+			UClass* AnimClass = humanCharacterAnimation.LoadSynchronous();
+			if (AnimClass && AnimClass->IsChildOf(UAnimInstance::StaticClass()))
+			{
+				// 유효한 애니메이션 클래스
+			}
+			else
+			{
+				// 유효하지 않은 클래스 처리
+				humanCharacterAnimation = nullptr;
+			}
+		}
+	}
+}
+#endif
