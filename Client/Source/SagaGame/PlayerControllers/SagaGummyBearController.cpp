f4838f23b6dc7c922ba95505adac121598280e85
iconstudio <yoyofa2@hotmail.com>
2024-08-15 19:58:36 +0900

    구미 베어 전용 컨트롤러의 클래스 추가

---
+++ Client/Source/SagaGame/PlayerControllers/SagaGummyBearController.cpp
@@ -0,0 +1,2 @@
+#include "PlayerControllers/SagaGummyBearController.h"
+
---
+++ Client/Source/SagaGame/PlayerControllers/SagaGummyBearController.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "SagaGame.h"
+#include <GameFramework/PlayerController.h>
+
+#include "SagaGummyBearController.generated.h"
+
+UCLASS(BlueprintType, Category = "CandyLandSaga|Game|System")
+class SAGAGAME_API ASagaGummyBearController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+};
